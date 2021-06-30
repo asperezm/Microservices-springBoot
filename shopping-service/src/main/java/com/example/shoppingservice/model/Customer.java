43a77d657396d5feb78a81ccb526a29cb9b4c0c1
asperezm <47333056+asperezm@users.noreply.github.com>
2021-06-30 11:00:19 -0500

    hystrix

--- shopping-service/src/main/java/com/example/shoppingservice/model/Customer.java
+++ shopping-service/src/main/java/com/example/shoppingservice/model/Customer.java
@@ -1,17 +1,8 @@
 package com.example.shoppingservice.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Builder;
 import lombok.Data;
-
-import javax.persistence.Column;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Data
 @Builder
